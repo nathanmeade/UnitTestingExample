aaaef1790d0195004e1b0989e1ec66fa57846c53
Nathan <nathan.r.meade@gmail.com>
2020-05-28 10:22:05 -0400

    feat: Room database instrumentation test implemented

---
+++ app/src/androidTest/java/com/meadetechnologies/unittestingexample/NoteDatabaseTest.java
@@ -0,0 +1,34 @@
+package com.meadetechnologies.unittestingexample;
+
+import android.app.Application;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.meadetechnologies.unittestingexample.persistence.NoteDao;
+import com.meadetechnologies.unittestingexample.persistence.NoteDatabase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class NoteDatabaseTest {
+
+    //system under test
+    private NoteDatabase noteDatabase;
+
+    public NoteDao getNoteDao(){
+        return noteDatabase.getNoteDao();
+    }
+
+    @Before
+    public void init(){
+        noteDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(), NoteDatabase.class
+        ).build();
+    }
+
+    @After
+    public void finish(){
+        noteDatabase.close();
+    }
+}
