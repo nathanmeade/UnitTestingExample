b44aa5bdec653b91b6945c8a05b87855d3570731
Nathan <nathan.r.meade@gmail.com>
2020-05-30 11:13:07 -0400

    feat: NoteViewModelTest implemented

---
+++ app/src/test/java/com/meadetechnologies/unittestingexample/ui/note/NoteViewModelTest.java
@@ -0,0 +1,130 @@
+package com.meadetechnologies.unittestingexample.ui.note;
+
+import com.meadetechnologies.unittestingexample.models.Note;
+import com.meadetechnologies.unittestingexample.repository.NoteRepository;
+import com.meadetechnologies.unittestingexample.ui.Resource;
+import com.meadetechnologies.unittestingexample.util.InstantExecutorExtension;
+import com.meadetechnologies.unittestingexample.util.LiveDataTestUtil;
+import com.meadetechnologies.unittestingexample.util.TestUtil;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import io.reactivex.Flowable;
+import io.reactivex.internal.operators.single.SingleToFlowable;
+
+import static com.meadetechnologies.unittestingexample.repository.NoteRepository.INSERT_SUCCESS;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(InstantExecutorExtension.class)
+public class NoteViewModelTest {
+
+    // system under test
+    private NoteViewModel noteViewModel;
+
+    @Mock
+    private NoteRepository noteRepository;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        noteViewModel = new NoteViewModel(noteRepository);
+    }
+
+    /*
+        can't observe a note that hasn't been set
+     */
+    @Test
+    void observeNote_whenNotSet() throws Exception {
+        //Arrange
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        //Act
+        Note note = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        //Assert
+        assertNull(note);
+    }
+    
+    /*
+        observe a note that has been set and onChanged will trigger in activity
+     */
+
+    @Test
+    void observeNote_whenSet() throws Exception {
+        //Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        //Act
+        noteViewModel.setNote(note);
+        Note observedNote = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        //Assert
+        assertEquals(note, observedNote);
+    }
+/*
+        insert a new note and observe row returned
+     */
+
+    @Test
+    void insertNote_returnRow() throws Exception {
+        //Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        final int insertedRow = 1;
+        Flowable<Resource<Integer>> returnedData = SingleToFlowable.just(Resource.success(insertedRow, INSERT_SUCCESS));
+        Mockito.when(noteRepository.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        //Act
+        noteViewModel.setNote(note);
+        Resource<Integer> returnedValue = liveDataTestUtil.getValue(noteViewModel.insertNote());
+
+        //Assert
+        assertEquals(Resource.success(insertedRow, INSERT_SUCCESS), returnedValue);
+    }
+/*
+        insert: don't return a new row without observer
+     */
+
+    @Test
+    void dontReturnInsertRowWithoutObserver() throws Exception {
+        //Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        //Act
+        noteViewModel.setNote(note);
+
+        //Assert
+        verify(noteRepository, never()).insertNote(any(Note.class));
+    }
+    /*
+        set note, null title, throw exception
+     */
+
+    @Test
+    void setNote_nullTitle_throwException() throws Exception {
+        //Arrange
+        final Note note = new Note(TestUtil.TEST_NOTE_1);
+        note.setTitle(null);
+
+        //Assert
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                //Act
+                noteViewModel.setNote(note);
+            }
+        });
+    }
+}
