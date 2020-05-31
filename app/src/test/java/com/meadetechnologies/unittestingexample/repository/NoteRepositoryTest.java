40feb75db8c36ae84bc5604991bc7e8e8fc346c8
Nathan <nathan.r.meade@gmail.com>
2020-05-31 15:41:02 -0400

    feat: Retrieving list of notes and deleting notes tests implemented for NoteRepository

--- app/src/test/java/com/meadetechnologies/unittestingexample/repository/NoteRepositoryTest.java
+++ app/src/test/java/com/meadetechnologies/unittestingexample/repository/NoteRepositoryTest.java
@@ -1,16 +1,25 @@
 package com.meadetechnologies.unittestingexample.repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.meadetechnologies.unittestingexample.models.Note;
 import com.meadetechnologies.unittestingexample.persistence.NoteDao;
 import com.meadetechnologies.unittestingexample.ui.Resource;
+import com.meadetechnologies.unittestingexample.util.InstantExecutorExtension;
+import com.meadetechnologies.unittestingexample.util.LiveDataTestUtil;
 import com.meadetechnologies.unittestingexample.util.TestUtil;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Single;
 
@@ -21,6 +30,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(InstantExecutorExtension.class)
 public class NoteRepositoryTest {
 
     private static final Note NOTE1 = new Note(TestUtil.TEST_NOTE_1);
@@ -166,5 +176,105 @@ public class NoteRepositoryTest {
             }
         });
         assertEquals(NOTE_TITLE_NULL, exception.getMessage());
+    }
+
+    /*
+        delete note
+        null id
+        throw exception
+     */
+
+    @Test
+    void deleteNote_nullId_throwException() throws Exception {
+        Exception exception = assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                final Note note = new Note(TestUtil.TEST_NOTE_1);
+                note.setId(-1);
+                noteRepository.deleteNote(note);
+            }
+        });
+
+        assertEquals(exception.getMessage(), INVALID_NOTE_ID);
+    }
+    /*
+        delete note
+        delete success
+        return Resource.success with deleted row
+     */
+
+    @Test
+    void deleteNote_deleteSuccess_returnResourceSuccess() throws Exception {
+        //Arrange
+        final int deletedRow = 1;
+        Resource<Integer> successResponse = Resource.success(deletedRow, DELETE_SUCCESS);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        when(noteDao.deleteNote(any(Note.class))).thenReturn(Single.just(deletedRow));
+
+        //Act
+        Resource<Integer> observedResource = liveDataTestUtil.getValue(noteRepository.deleteNote(NOTE1));
+
+        //Assert
+        assertEquals(observedResource, successResponse);
+    }
+    /*
+        delete note
+        delete failure
+        return Resource.error
+     */
+    @Test
+    void deleteNote_deleteFailure_returnResourceSuccess() throws Exception {
+        //Arrange
+        final int deletedRow = -1;
+        Resource<Integer> failureResponse = Resource.error(deletedRow, DELETE_FAILURE);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        when(noteDao.deleteNote(any(Note.class))).thenReturn(Single.just(deletedRow));
+
+        //Act
+        Resource<Integer> observedResource = liveDataTestUtil.getValue(noteRepository.deleteNote(NOTE1));
+
+        //Assert
+        assertEquals(observedResource, failureResponse);
+    }
+
+    /*
+        retrieve notes
+        return list of notes
+     */
+
+    @Test
+    void getNotes_returnListWithNotes() throws Exception {
+        //Arrange
+        List<Note> notes = TestUtil.TEST_NOTES_LIST;
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<List<Note>> returnedData = new MutableLiveData<>();
+        returnedData.setValue(notes);
+        when(noteDao.getNotes()).thenReturn(returnedData);
+
+        //Act
+        List<Note> observedData = liveDataTestUtil.getValue(noteRepository.getNotes());
+
+        //Assert
+        assertEquals(observedData, returnedData.getValue());
+    }
+    /*
+        retrieve notes
+        return empty list
+     */
+
+    @Test
+    void getNotes_returnEmptyList() throws Exception {
+        //Arrange
+        List<Note> notes = new ArrayList<>();
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<List<Note>> returnedData = new MutableLiveData<>();
+        returnedData.setValue(notes);
+        when(noteDao.getNotes()).thenReturn(returnedData);
+
+        //Act
+        List<Note> observedData = liveDataTestUtil.getValue(noteRepository.getNotes());
+
+        //Assert
+        assertEquals(observedData, returnedData.getValue());
     }
 }
